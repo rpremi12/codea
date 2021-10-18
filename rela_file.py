2434666feed3d08b813fca0cdfb970a869f088db
herebcirelapsed <rela.vergis@gmail.com>
2021-10-17 19:31:44 -0700

    Finished adding df, will soon add reading_time feature

--- rela_file.py
+++ rela_file.py
@@ -6,15 +6,37 @@ Created on Sat Oct 16 21:24:42 2021
 """
 
 import arxiv
+import pandas as pd
 
-def search(query_terms):
-  search = arxiv.Search(
-    query = query_terms,
+def list_to_string(target):
+    string = ''
+    for substring in target:
+        new_string = '{} '.format(substring)
+        string = string + new_string
+    return(string)
+
+
+def search(*query_terms):
+    data = []
+    search = arxiv.Search(
+    query = list_to_string(query_terms),
     max_results = 10,
     sort_by = arxiv.SortCriterion.Relevance
   )
 
-  for result in search.results():
-    print(result.title)
+    for result in search.results():
+            data.append({
+            'id': result.entry_id.split('/')[-1],
+            'url': result.pdf_url,
+            'title': result.title,
+            'image': None,
+            'claps': None,
+            'responses': None,
+            'reading_time': None,
+            'publication': result.published,
+            'date': result.updated
+            })
+    return pd.DataFrame(data)
 
-search('python machine learning')
+
+search('python', 'machine learning')
